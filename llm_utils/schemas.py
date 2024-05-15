fbd18bdb15b7b1376a6f830382a972fc8975e30e
ehddnr301 <dy950328@gmail.com>
2024-05-15 13:38:06 +0000

    Update Schema
    Schema의 description 부분을 한글로 작성하였고 좀 더 자세히 작성하였습니다.

--- llm_utils/schemas.py
+++ llm_utils/schemas.py
@@ -4,5 +4,11 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 class PlaceAndFood(BaseModel):
     """Place And Food."""
 
-    place: str = Field(description="The name of the area from the conversation")
-    food: str = Field(description="The name of the food from the conversation")
+    place: str = Field(
+        description="대화에서 나온 확실한 지역명 필드. 대화에서 지역명이 확실히 지정되지 않은 경우, '강남역'을 사용한다."
+    )
+    food: str = Field(
+        description="""
+            대화에서 나온 확실한 음식명 필드. 대화에서 음식명이 확실히 지정되지 않은 경우, 음식카테고리중 하나를 골라 사용한다: '패스트 푸드', '한식', '일식', '중식', '베트남식', '맛집'
+        """
+    )
